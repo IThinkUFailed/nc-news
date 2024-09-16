a592cfbb12ef40eda069b007e26c2d675578d37b
Mark <mark@Marks-MacBook-Pro.local>
2024-09-16 15:23:45 +0100

    Task 4: Retrieving all articles

---
+++ src/Components/AllArticles.jsx
@@ -0,0 +1,31 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
+let url = "https://nc-news-px9w.onrender.com/api/articles"
+
+const AllArticles = () => {
+const [isLoading, setIsLoading] = useState(true)
+const [results, setResults] = useState([])
+    useEffect(()=> {
+        axios.get(url).then(({data})=>{
+            setResults(data.articles)
+            console.log(data.articles)
+            setIsLoading(false)
+        })
+      },[]);
+
+      if (isLoading) return <p>Loading...</p>;
+      
+      return (
+        <section className="articles-container">
+          {results.map((article, index) => (
+            <div key={article.article_id || index} className="article-div">
+              <h3>{article.title}</h3>
+              <img src={article.article_img_url} alt={article.title} />
+            </div>
+          ))}
+        </section>
+      )
+    }
+
+export default AllArticles
